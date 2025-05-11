85f5b5eb76031dfb803de40a4ca72209686c9f5f
josecabellog <josegregoriocabellog@gmail.com>
2025-05-11 14:26:47 -0400

    first commit venv

---
+++ videogameweb/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for videogameweb project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'videogameweb.settings')
+
+application = get_asgi_application()
