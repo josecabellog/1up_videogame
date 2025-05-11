695f3551340abb4add85b67c9b5bc38a9bc471f7
josecabellog <josegregoriocabellog@gmail.com>
2025-05-11 15:59:58 -0400

    paginas added, pero sin css

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, AboutPageView, CategoriesPageView, ContactPageView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home')
+    path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name= 'about'),
+    path('categories/', CategoriesPageView.as_view(), name= 'categories'),
+    path('contact/', ContactPageView.as_view(), name= 'contact'),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -5,3 +5,12 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'index.html'
+
+class AboutPageView(TemplateView):
+    template_name= 'about.html'
+
+class CategoriesPageView(TemplateView):
+    template_name= 'categories.html'
+
+class ContactPageView(TemplateView):
+    template_name= 'contact.html'
